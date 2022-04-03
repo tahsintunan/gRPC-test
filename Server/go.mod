c76227e43d33677e8701937b080155b197200d15
Tahsin Tunan <tahsintunan@gmail.com>
2022-04-03 17:36:01 +0600

    Database connection established

--- Server/go.mod
+++ Server/go.mod
@@ -3,14 +3,16 @@ module server
 go 1.18
 
 require (
+	github.com/lib/pq v1.10.4
 	google.golang.org/grpc v1.45.0
 	google.golang.org/protobuf v1.28.0
 )
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
-	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
-	golang.org/x/text v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
--- Server/server.go
+++ Server/server.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net"
+
+	// "os"
 	pb "server/protos/user"
 
+	_ "github.com/lib/pq"
+
+	// "github.com/jackc/pgx/v4"
 	"google.golang.org/grpc"
+	// "golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -37,7 +44,58 @@ func (s *userAuthServer) Logout(ctx context.Context, in *pb.LogoutReq) (*pb.ApiR
 	return &pb.ApiRes{ResCode: 200, Message: "successfully logged out"}, nil
 }
 
+// func verifyRegReq(in *pb.RegReq) error {
+// 	if in.GetUsername() == "" {
+// 		return fmt.Errorf("username is empty")
+// 	}
+// 	if in.GetPassword() == "" {
+// 		return fmt.Errorf("password is empty")
+// 	}
+// 	return nil
+// }
+
+// func verifyLoginReq(in *pb.LoginReq) error {
+// 	if in.GetUsername() == "" {
+// 		return fmt.Errorf("username is empty")
+// 	}
+// 	if in.GetPassword() == "" {
+// 		return fmt.Errorf("password is empty")
+// 	}
+// 	return nil
+// }
+
+// type user struct{
+// 	email string
+// 	username string
+// 	password string
+// }
+
+func initDbConn() {
+	db_url := "postgresql://postgres:postgres@localhost:5432/postgres"
+	conn, err := sql.Open("postgres", db_url)
+	if err != nil {
+		log.Fatalf("could not open postgresql connection: %v", err)
+	}
+	log.Printf("Connected to database at port 5432")
+	defer conn.Close()
+}
+
 func main() {
+	db_url := "postgresql://postgres:password@localhost:5432/grpc_assignment_db"
+	conn, err := sql.Open("postgres", db_url)
+	if err != nil {
+		log.Fatalf("could not open postgresql connection: %v", err)
+	}
+	log.Printf("Connected to database at port 5432")
+	defer conn.Close()
+	// initDbConn()
+
+	data, err := conn.Query("SELECT * FROM user")
+	if err != nil {
+		log.Fatalf("could not query database: %v", err)
+	}
+	defer data.Close()
+
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
